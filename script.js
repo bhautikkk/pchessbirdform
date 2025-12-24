464e0d22b263d3be57bfb07ff81ac2a10a4a2df6
bhautik <bhautik@example.com>
2025-12-24 15:37:13 +0530

    Update event ID to event_test_reset_01.07

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Change false to true to OPEN registration
     // ==========================================
     const isRegistrationOpen = true;
-    const eventId = 'event_test_reset_01.06'; // CHANGE THIS FOR NEW EVENTS
+    const eventId = 'event_test_reset_01.07'; // CHANGE THIS FOR NEW EVENTS
     // ==========================================
 
     const form = document.getElementById('chessForm');
